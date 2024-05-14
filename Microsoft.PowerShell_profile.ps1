f9eb146a01a1bbe50be5e3aa3dbdb9574532ec31
Diego Xavier <62605851+diegoxav@users.noreply.github.com>
2024-05-14 17:06:19 -0300

    Update Microsoft.PowerShell_profile.ps1

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,7 +1,7 @@
 ### PowerShell Profile Refactor
 ### Version 1.03 - Refactored
 cls
-fastfetch -l "Windows 7"
+fastfetch -l "Windows 11"
 
 # Initial GitHub.com connectivity check with 1 second timeout
 $canConnectToGitHub = Test-Connection github.com -Count 1 -Quiet -TimeoutSeconds 1
@@ -45,7 +45,7 @@ Update-FastFetch
 
 # Display Fastfetch
 cls
-fastfetch -l "Windows 7"
+fastfetch -l "Windows 11"
 
 # Check for Profile Updates
 function Update-Profile {
